c83325fe9d6b039d8b2452de61189f18a6975b92
umairuk0316 <umairuk0316@gmail.com>
2026-01-27 13:38:01 -0800

    change routing

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.querySelector(".menu-toggle");
   const menuList = document.querySelector(".list");
   const icons = document.querySelector(".icons");
+  const burger = document.getElementById("burger");
+  const menu = document.getElementById("menu");
 
   toggleBtn.addEventListener("click", () => {
     menuList.classList.toggle("active");
     icons.classList.toggle("show-icons");
+  });
+  
+  burger.addEventListener("click", () => {
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
   });
 });
 
